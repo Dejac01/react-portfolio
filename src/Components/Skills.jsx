783c0cc9ddab3ea57f542d4f958a4792d1c4c2c0
deja.craft <dejacraft28@gmail.com>
2023-06-05 11:50:18 -0400

    updates

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -19,7 +19,7 @@ const Skills = () => {
       {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p className="text-5xl font-bold inline border-b-4 border-pink-300 ">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-300 ">
             Skills
           </p>
           <p className="py-4">
