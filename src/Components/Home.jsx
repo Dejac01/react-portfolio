ec633ac5bef3042b6cf52b9d8e8ce53761a46e8f
deja.craft <dejacraft28@gmail.com>
2023-06-05 02:23:00 -0400

    Portfolio

---
+++ src/Components/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#5e636a]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-300">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          De`ja Craft
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Software Developer, Writer & Digital Marketer.
+        </h2>
+        <p className="text-pink-300 py-4 max-w-[700px]">
+          I specialize in building and designing exceptional digital
+          experiences. Take a look around!
+        </p>
+        <div>
+          <button className="text-pink-300 group border-2 px-6 py-3 my-2 flex items-center hover:bg-black hover:border-pink-300">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Components/Skills.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import HTML from "../Assets/html.png";
+import CSS from "../Assets/css.png";
+import JavaScript from "../Assets/javascript.png";
+import ReactImg from "../Assets/react.png";
+import Node from "../Assets/node.png";
+import GitHub from "../Assets/github.png";
+import Tailwind from "../Assets/tailwind.png";
+import Mongo from "../Assets/mongo.png";
+import Figma from "../Assets/figma.png";
+import Canva from "../Assets/canva.png";
+import Servicenow from "../Assets/servicenow.png";
+
+const Skills = () => {
+  return (
+    <div name="skills" className="w-full h-screen bg-[#5e636a] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-300 ">
+            Skills
+          </p>
+          <p className="py-4">
+            // Checkout a few of the technologies and softwares I am skilled in!
+          </p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+            <p className="my-4">HTML</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={CSS} alt="HTML icon" />
+            <p className="my-4">CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
+            <p className="my-4">JAVASCRIPT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
+            <p className="my-4">REACT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
+            <p className="my-4">GITHUB</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Node} alt="HTML icon" />
+            <p className="my-4">NODE JS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Mongo} alt="HTML icon" />
+            <p className="my-4">MONGO DB</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
+            <p className="my-4">TAILWIND</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Figma} alt="HTML icon" />
+            <p className="my-4">Figma</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Canva} alt="HTML icon" />
+            <p className="my-4">Canva</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-40 mx-auto" src={Servicenow} alt="HTML icon" />
+            <p className="my-4">Service-Now</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
